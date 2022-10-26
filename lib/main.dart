00eacd6cda29e286b2a51e985a392b202819460c
mtayyabb <mmuhammadttayyab@gmail.com>
2022-10-26 13:25:14 +0500

    Login, Log Out, Splash Screen,Storing, fetching and removing value using Shared Preference

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-//remove debug red banner
+//remove debugger red banner
       title: 'Shared Preferences',
 
       theme: ThemeData(
